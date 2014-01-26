88a270c3ea33945b7cd13c72a9416b9c77fa013f
Djulz <juliussandgren@hotmail.com>
2014-01-26 14:27:15 +0100

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		if(Input.GetKey(KeyCode.A))
+			transform.position += new Vector3 (0, 0.05f, 0);
+	}
+}
